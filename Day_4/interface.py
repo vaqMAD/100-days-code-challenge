65313e108085b398502454bf3e8f14a13b664f69
Damian Ignaczak <vaqmad@icloud.com>
2022-11-28 16:39:56 +0100

    Resolve Day_4 project. Add .gitignore file
    Resolve Day_4 project - create simple program based on function which will imitate the rock paper scissors game

---
+++ Day_4/interface.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Interface(Enum):
+    Rock = "Rock"
+    Paper = "Paper"
+    Scissors = "Scissors"
+
+
---
+++ Day_4/main.py
@@ -0,0 +1,46 @@
+import random
+from interface import Interface
+
+def rps_game():
+
+    possible_choices = [Interface.Rock.value,
+                        Interface.Paper.value, Interface.Scissors.value]
+
+    user_choice = input(
+        "What do you choose? Please type : 'Rock' for Rock, 'Paper' for Paper, 'Scissors' for Scissors ").capitalize()
+
+    computer_choice = random.choice(possible_choices)
+
+    if user_choice not in possible_choices:
+        raise ValueError(
+            f"Given value {user_choice} is incorrect! Please type : 'Rock' for Rock, 'Paper' for Paper, 'Scissors' for Scissors")
+    else:
+        if computer_choice == user_choice:
+            print(
+                f"You picked : {user_choice}, computer picked : {computer_choice}. It's a draw!")
+        elif user_choice == Interface.Rock.value:
+            if computer_choice == Interface.Paper.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. Computer wins!")
+            elif computer_choice == Interface.Scissors.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. You win!")
+
+        elif user_choice == Interface.Paper.value:
+            if computer_choice == Interface.Rock.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. You win!")
+            elif computer_choice == Interface.Scissors.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. Computer wins!")
+
+        elif user_choice == Interface.Scissors.value:
+            if computer_choice == Interface.Rock.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. Computer wins!")
+            elif computer_choice == Interface.Paper.value:
+                print(
+                    f"You picked : {user_choice}, computer picked : {computer_choice}. You win!")
+
+
+rps_game()
