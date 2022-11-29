d89c86fa373f88ea05d3f6f85245d93fbd3a2ef4
Damian Ignaczak <vaqmad@icloud.com>
2022-11-29 18:57:19 +0100

    Resolve Day_5 project. Create simple password generator program
    Resolve Day_5 project - create simple password generator which will generate password by given password parameters (how many characters each type you want).

---
+++ Day_5/main.py
@@ -0,0 +1,39 @@
+import string
+import random
+
+
+def password_generator():
+    lower_case = list(string.ascii_lowercase)
+    upper_case = list(string.ascii_uppercase)
+    digits = list(string.digits)
+    punctuation = list(string.punctuation)
+
+    password_lower_case_letters = int(
+        input("How many lower case letters would you like in your password ? \n"))
+    password_upper_case_letters = int(
+        input("How many upper case letters would you like in your password ? \n"))
+    password_digits = int(
+        input("How many digits would you like in your password ? \n"))
+    punctuation_characters = int(
+        input("How many punctuation characters would you like in your password ? \n"))
+
+    finall_password = []
+
+    for _ in range(password_lower_case_letters):
+        finall_password.append(random.choice(lower_case))
+
+    for _ in range(password_upper_case_letters):
+        finall_password.append(random.choice(upper_case))
+
+    for _ in range(password_digits):
+        finall_password.append(random.choice(digits))
+
+    for _ in range(punctuation_characters):
+        finall_password.append(random.choice(punctuation))
+
+    random.shuffle(finall_password)
+
+    return finall_password
+
+
+print(password_generator())
