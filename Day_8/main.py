b05e3dee8d9ebb131990b783bfa338d4d20b0595
Damian Ignaczak <vaqmad@icloud.com>
2022-12-03 23:05:26 +0100

    Add solution for decode message by Cesar Cipher

---
+++ Day_8/main.py
@@ -0,0 +1,40 @@
+import string
+
+
+def encode_message(message: str, letter_shift: int):
+    alfabeth = list(string.ascii_lowercase)
+    encoded_letters = []
+
+    for message_letter in message:
+        message_letter = message_letter.lower()
+        if message_letter == " ":
+            encoded_letters.append(message_letter)
+        else:
+            for position in range(len(alfabeth)):
+                if message_letter == alfabeth[position]:
+                    try:
+                        shifted_index = position + letter_shift
+                        endoded_shifted_letter = alfabeth[shifted_index]
+                        encoded_letters.append(endoded_shifted_letter)
+                    except IndexError:
+                        shifted_index = (
+                            position - len(alfabeth)) + letter_shift
+                        endoded_shifted_letter = alfabeth[shifted_index]
+                        encoded_letters.append(endoded_shifted_letter)
+                elif message_letter not in alfabeth:
+                    raise AttributeError(
+                        f'Given character:"{message_letter}" do not belong to alfabeth')
+
+    endcoded_message = "".join(encoded_letters)
+    return endcoded_message
+
+
+print(encode_message("Nikola dupa xdd", 26))
+
+
+def decode_message():
+    pass
+
+
+def main_program():
+    pass
