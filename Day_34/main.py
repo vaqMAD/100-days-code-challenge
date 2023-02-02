54b4f99750848c735a1833905d44dc1d78df2037
Damian Ignaczak <vaqmad@icloud.com>
2023-02-02 21:40:43 +0100

    Resolve Day_34 project - GUI Quiz App - based on project requirements
    Create QuizBrain class to get data from opentdb API
    Create QuizInterface class to view questions in GUI and give the user the option to choose between true and false

---
+++ Day_34/main.py
@@ -0,0 +1,9 @@
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+quiz = QuizBrain()
+quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.ask_question()
+
